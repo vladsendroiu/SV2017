40b762525aad9efb5a95efe5c71c8b05c333a078
VladSendroiu <vlad_sendroiu@yahoo.com>
2017-04-11 10:54:47 -0700

    after lab 2

---
+++ WebApplication1/Controllers/MinesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class MinesController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+        // GET: Mines
+        public ActionResult Index()
+        {
+            var userId = this.User.Identity.GetUserId();
+            var user = dbContext.Users.Find(userId);
+            var city = user.Cities.First();
+            return View(city);
+        }
+        public ActionResult Details(int mineId)
+        {
+            var userId = this.User.Identity.GetUserId();
+            var user = dbContext.Users.Find(userId);
+            var city = user.Cities.First();
+            var mine = dbContext.Mines.Find(mineId);
+            return View(mine);
+        }
+    }
+}
